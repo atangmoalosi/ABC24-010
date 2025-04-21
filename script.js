0095919f571d31967eeb6046e62707ae1a417729
atangmoalosi <atanggmoalosi@gmail.com>
2025-04-21 12:28:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+const imageList = [
+  'images/women-top1.jpg',
+  'images/women-top2.jpg',
+  'images/women-top3.jpg'
+];
+
+let currentIndex = 0;
+const imgElement = document.getElementById('carousel-image');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+
+function showImage(index) {
+  imgElement.src = imageList[index];
+}
+
+prevBtn.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + imageList.length) % imageList.length;
+  showImage(currentIndex);
+});
+
+nextBtn.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % imageList.length;
+  showImage(currentIndex);
+});
 
