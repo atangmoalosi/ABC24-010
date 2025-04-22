38eb5233246a857f3e00e2ace50944b89631acb0
atangmoalosi <atanggmoalosi@gmail.com>
2025-04-22 05:30:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,52 @@
-const imageList = [
-   {
-     src: 'images/strapless top.jpg' ,
-     title: 'Strapless Top'
-  },
-  {
-    src: 'images/blackfloralhaltertop.jpg',
-    title: 'Black Floral Halter Top'
-  },
-  {
-    src: 'images/orangedresstop.jpg',
-    title: 'Orange Dress Top'
-  },
-   {
-    src: 'images/leopardprinttop.jpg',
-    title: 'Leopard Print Top'
-   },
-   {
-    src: 'images/lacetop.jpg',
-    title: 'Lace Top'
-   },
-   {
-    src: 'images/meshtop.jpg',
-    title: 'Mesh Top'
-   },
-   {
-    src: 'images/vneckleopardtop.jpg',
-    title: 'V-Neck Leopard Top'
-   },
-   {
-    src: 'images/dazzlingredtop.jpg',
-    title: 'Dazzling Red Top'
-   },
-   {
-    src: 'images/tightlongsleeve.jpg',
-    title: 'Tight Long Sleeve Top'
-   },
-   {
-    src: 'images/stripedlongsleeve.jpg',
-    title: 'Striped Long Sleeve Top'
-   }
-   
-];
+function createCarousel(images, imgId, titleId, prevClass, nextClass) {
+  let index = 0;
+  const imgEl = document.getElementById(imgId);
+  const titleEl = document.getElementById(titleId);
+  const prevBtn = document.querySelector(`.${prevClass}`);
+  const nextBtn = document.querySelector(`.${nextClass}`);
 
-let currentIndex = 0;
-const titleElement = document.getElementById('carousel-title');
-const imgElement = document.getElementById('carousel-image');
-const prevBtn = document.querySelector('.prev');
-const nextBtn = document.querySelector('.next');
+  function show() {
+    imgEl.src = images[index].src;
+    titleEl.textContent = images[index].title;
+  }
 
-function showImage(index) {
-  imgElement.src = imageList[index].src;
-  titleElement.textContent = imageList[index].title;
+  prevBtn.addEventListener('click', () => {
+    index = (index - 1 + images.length) % images.length;
+    show();
+  });
+
+  nextBtn.addEventListener('click', () => {
+    index = (index + 1) % images.length;
+    show();
+  });
+
+  show(); // show first image
 }
 
-prevBtn.addEventListener('click', () => {
-  currentIndex = (currentIndex - 1 + imageList.length) % imageList.length;
-  showImage(currentIndex);
-});
+// Carousel 1: Tops
+createCarousel([
+  { src: 'images/strapless top.jpg', title: 'Strapless Top' },
+  { src: 'images/blackfloralhaltertop.jpg', title: 'Black Floral Halter Top' }
+  {src: 'images/orangedresstop.jpg', title: 'Orange Dress Top' },
+  {src: 'images/leopardprinttop.jpg',title: 'Leopard Print Top'},
+  {src: 'images/lacetop.jpg', title: 'Lace Top'},
+  {src: 'images/meshtop.jpg',title: 'Mesh Top'},
+  {src: 'images/vneckleopardtop.jpg',title: 'V-Neck Leopard Top'},
+  {src: 'images/dazzlingredtop.jpg',title: 'Dazzling Red Top'},
+  {src: 'images/tightlongsleeve.jpg',title: 'Tight Long Sleeve Top'},
+  {src: 'images/stripedlongsleeve.jpg',title: 'Striped Long Sleeve Top'}
+  
+          
+], 'tops-image', 'tops-title', 'tops-prev', 'tops-next');
 
-nextBtn.addEventListener('click', () => {
-  currentIndex = (currentIndex + 1) % imageList.length;
-  showImage(currentIndex);
-});
-showImage(currentIndex);
+// Carousel 2: Skirts
+createCarousel([
+  { src: 'images/skirt1.jpg', title: 'Denim Skirt' },
+  { src: 'images/skirt2.jpg', title: 'Floral Skirt' }
+], 'skirts-image', 'skirts-title', 'skirts-prev', 'skirts-next');
 
+// Carousel 3: Pants
+createCarousel([
+  { src: 'images/pants1.jpg', title: 'Black Pants' },
+  { src: 'images/pants2.jpg', title: 'Beige Pants' }
+], 'pants-image', 'pants-title', 'pants-prev', 'pants-next');
